8bf163f529a2ee7b610624cece15df5b0155e53c
tutyvp <126392885+tutyvp@users.noreply.github.com>
2023-04-07 15:43:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 460411293 # Ваш chat ID, не меняйте название переменной
+
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+# v = a*t
+t = 10
+v = np.min(x) + 37
+return v/t
